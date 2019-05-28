8db64ff4ade0669ea6a88f5f10f7a563a95b3542
hiepxuan2008 <hiepxuan2008@gmail.com>
2019-05-29 00:48:30 +0700

    upload solutions for DP1

---
+++ 24. Dynamic Programming 1/3 [uva] 11137 - Ingenuous Cubrency.cpp
@@ -0,0 +1,33 @@
+﻿// Problem: 11137 - Ingenuous Cubrency
+// Link: https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2078
+// Author: Mai Thanh Hiep
+// Complexity: O(COIN_SIZE * MAXN); COIN_SIZE = 21, MAXN = 10^4
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef long long ll;
+#define COIN_SIZE 21
+#define MAXN 10000
+
+int main() {
+	vector<int> coins(COIN_SIZE + 1);
+	for (int i = 1; i <= COIN_SIZE; i++)
+		coins[i] = i * i * i;
+
+	vector<ll> dp(MAXN, 0);
+	dp[0] = 1;
+	for (int i = 1; i <= COIN_SIZE; i++) {
+		for (int j = coins[i]; j < MAXN; j++) {
+			dp[j] += dp[j - coins[i]];
+		}
+	}
+
+	int n;
+	while (cin >> n) {
+		cout << dp[n] << endl;
+	}
+
+	return 0;
+}
---
+++ 24. Dynamic Programming 1/5 [spoj] ACODE - Alphacode.cpp
@@ -0,0 +1,34 @@
+﻿// Problem: ACODE - Alphacode
+// Link: https://www.spoj.com/problems/ACODE/
+// Author: Mai Thanh Hiep
+// Complixity: T * O(N), N <= MAX = 5000
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define MAX 5001
+
+int main() {
+	string s;
+	int n;
+	long long dp[MAX];
+	while (cin >> s && s != "0") {
+		n = s.size();
+		dp[0] = 1;
+		int index;
+		for (int i = 1; i <= n; i++) {
+			dp[i] = 0;
+			index = i - 1;
+			if (s[index] != '0')
+				dp[i] += dp[i - 1];
+			if (index - 1 >= 0 && (s[index-1] == '1' || s[index-1] == '2' && s[index] <= '6'))
+				dp[i] += dp[i - 2];
+		}
+		cout << dp[n] << endl;
+	}
+
+	return 0;
+}
