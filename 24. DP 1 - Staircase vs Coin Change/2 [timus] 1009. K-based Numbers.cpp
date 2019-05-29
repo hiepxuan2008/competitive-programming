92ce1cc6b1a29e8c74174492c27d0e81cdbb6ed5
hiepxuan2008 <hiepxuan2008@gmail.com>
2019-05-29 16:17:14 +0700

    update dynamic programming solutions

---
+++ 24. DP 1 - Staircase vs Coin Change/2 [timus] 1009. K-based Numbers.cpp
@@ -0,0 +1,32 @@
+﻿// Problem: 1009. K-based Numbers
+// Link: http://acm.timus.ru/problem.aspx?num=1009
+// Author: Mai Thanh Hiep
+// Complexity: O(N), N <= 16
+
+#include <iostream>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+struct Digit {
+	ll zeros;
+	ll nonZeros;
+};
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	vector<Digit> count(n);
+	count[0].zeros = 0; // The number can't be leading with 0
+	count[0].nonZeros = k - 1; // [1, k - 1]
+
+	for (int i = 1; i < n; i++) {
+		count[i].zeros = count[i - 1].nonZeros; // can't contain two successive zeros
+		count[i].nonZeros = (count[i - 1].nonZeros + count[i - 1].zeros) * (k - 1);
+	}
+
+	cout << (count[n - 1].zeros + count[n - 1].nonZeros) << endl;
+
+	return 0;
+}
---
+++ 27. DP 4 - Knapsack/1 [codechef] PPTEST - Polo the Penguin and the Test.cpp
@@ -0,0 +1,51 @@
+﻿// Problem: PPTEST - Polo the Penguin and the Test
+// Link: https://www.codechef.com/problems/PPTEST
+// Author: Mai Thanh Hiep
+// Complexity: T * O(N * W), T, N, W <= 100
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define MAXN 101
+#define MAXW 101
+int weights[MAXN];
+int profits[MAXN];
+
+// O(N * W)
+int solve01Knapsack(int n, int w) {
+	vector<vector<int>> dp(n + 1, vector<int>(w + 1, 0));
+	int itemIndex;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= w; j++) {
+			itemIndex = i - 1;
+			dp[i][j] = dp[i - 1][j];
+			if (j >= weights[itemIndex]) {
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - weights[itemIndex]] + profits[itemIndex]);
+			}
+		}
+	}
+	return dp[n][w];
+}
+
+int main() {
+	int T;
+	cin >> T;
+
+	int N, W;
+	int c, p, t;
+	while (T--) {
+		cin >> N >> W;
+		for (int i = 0; i < N; i++) {
+			cin >> c >> p >> t;
+			profits[i] = c * p;
+			weights[i] = t;
+		}
+
+		cout << solve01Knapsack(N, W) << endl;
+	}
+
+	return 0;
+}
