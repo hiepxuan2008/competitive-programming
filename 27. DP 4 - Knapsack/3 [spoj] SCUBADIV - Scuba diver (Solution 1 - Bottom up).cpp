3d43fe4afb4c627ca657a716d37ac02d07a1fa0b
hiepxuan2008 <hiepxuan2008@gmail.com>
2019-06-02 19:00:09 +0700

    add bottom down solution

---
+++ 27. DP 4 - Knapsack/3 [spoj] SCUBADIV - Scuba diver (Solution 1 - Bottom up).cpp
@@ -0,0 +1,73 @@
+﻿// Problem: SCUBADIV - Scuba diver
+// Link: https://www.spoj.com/problems/SCUBADIV/
+// Author: Mai Thanh Hiep
+// Complexity: TC * O(N * T * A), N <= 1000, T <= 21, A <= 79
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define INF 800 * 1000 + 1
+#define MAX_OXYGEN 21
+#define MAX_INTROGEN 79
+#define MAX_N 1000
+
+int dp[MAX_N + 1][MAX_OXYGEN + 1][MAX_INTROGEN + 1];
+int oxygens[MAX_N], nitrogens[MAX_N], weights[MAX_N];
+
+// O(N * T * A)
+int solve01Knapsack(int n, int oxygenNeed, int nitrogenNeed) {
+    int itemIndex;
+    int weight;
+    int _oxyNeed;
+    int _nitroNeed;
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= oxygenNeed; j++) {
+            for (int k = 0; k <= nitrogenNeed; k++) {
+                if (i == 0) { // no item
+                    if (j == 0 && k == 0) {
+                        dp[i][j][k] = 0;
+                    } else {
+                        dp[i][j][k] = INF; // can't feed i, j
+                    }
+                } else {
+                    itemIndex = i - 1;
+
+                    // if we don't take item itemIndex
+                    dp[i][j][k] = dp[i - 1][j][k];
+
+                    // if we take item itemIndex
+                    _oxyNeed = max(j - oxygens[itemIndex], 0);
+                    _nitroNeed = max(0, k - nitrogens[itemIndex]);
+                    weight = dp[i - 1][_oxyNeed][_nitroNeed];
+                    if (weight != INF) {
+                        dp[i][j][k] = min(dp[i][j][k], weight + weights[itemIndex]);
+                    }
+                }
+            }
+        }
+    }
+
+    return dp[n][oxygenNeed][nitrogenNeed];
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    int oxygenNeed, nitrogenNeed, n;
+    while (T--) {
+        cin >> oxygenNeed >> nitrogenNeed;
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cin >> oxygens[i] >> nitrogens[i] >> weights[i];
+        }
+
+        cout << solve01Knapsack(n, oxygenNeed, nitrogenNeed) << endl;
+    }
+
+    return 0;
+}
---
+++ 27. DP 4 - Knapsack/3 [spoj] SCUBADIV - Scuba diver (Solution 2 - Top down).cpp
@@ -0,0 +1,69 @@
+﻿// Problem: SCUBADIV - Scuba diver
+// Link: https://www.spoj.com/problems/SCUBADIV/
+// Author: Mai Thanh Hiep
+// Complexity: TC * O(N * T * A), N <= 1000, T <= 21, A <= 79
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+#define INF 800 * 1000 + 1
+#define MAX_OXYGEN 21
+#define MAX_INTROGEN 79
+#define MAX_N 1000
+
+int dp[MAX_N + 1][MAX_OXYGEN + 1][MAX_INTROGEN + 1];
+int oxygens[MAX_N], nitrogens[MAX_N], weights[MAX_N];
+
+// O(N * T * A)
+int solve01Knapsack(int n, int oxygenNeed, int nitrogenNeed) {
+    if (oxygenNeed == 0 && nitrogenNeed == 0)
+        return 0; // no weight need
+
+    if (n == 0)
+        return INF; // no item -> can't feed oxygenNeed or nitrogenNeed
+
+    if (dp[n][oxygenNeed][nitrogenNeed] == -1) {
+        int itemIndex = n - 1;
+
+        // if we don't take item itemIndex
+        dp[n][oxygenNeed][nitrogenNeed] = solve01Knapsack(n - 1, oxygenNeed, nitrogenNeed);
+
+        // if we take item itemIndex
+        int _oxyNeed = max(oxygenNeed - oxygens[itemIndex], 0);
+        int _nitroNeed = max(nitrogenNeed - nitrogens[itemIndex], 0);
+        int weight = solve01Knapsack(n - 1, _oxyNeed, _nitroNeed);
+        if (weight != INF) {
+            dp[n][oxygenNeed][nitrogenNeed] = min(dp[n][oxygenNeed][nitrogenNeed], weight + weights[itemIndex]);
+        }
+    }
+
+    return dp[n][oxygenNeed][nitrogenNeed];
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    int oxygenNeed, nitrogenNeed, n;
+    while (T--) {
+        cin >> oxygenNeed >> nitrogenNeed;
+        cin >> n;
+
+        for (int i = 0; i < n; i++) {
+            cin >> oxygens[i] >> nitrogens[i] >> weights[i];
+        }
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= oxygenNeed; j++) {
+                for (int k = 0; k <= nitrogenNeed; k++)
+                    dp[i][j][k] = -1;
+            }
+        }
+
+        cout << solve01Knapsack(n, oxygenNeed, nitrogenNeed) << endl;
+    }
+
+    return 0;
+}
