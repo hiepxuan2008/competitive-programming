7ac1c8f5c21707f7346985d58d95754c72e75e40
hiepxuan2008 <hiepxuan2008@gmail.com>
2019-06-07 16:08:15 +0700

    update

--- 25. DP 2 - Longest Common Subsequence/5 [lightoj] 1013 - Love Calculator.cpp
+++ 25. DP 2 - Longest Common Subsequence/5 [lightoj] 1013 - Love Calculator.cpp
@@ -3,49 +3,38 @@
 // Author: Mai Thanh Hiep
 // Complexity: T * O(|str1| * |str2|); T <= 125, |str1|, |str2| <= 30
 
-// https://github.com/sameer2800/Lightoj-Solutions/blob/master/1013%20-%20Love%20Calculator.cpp
-// https://github.com/shahidul2k9/problem-solution/blob/master/lightoj/1013%20-%20Love%20Calculator.cpp
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <string>
 using namespace std;
+typedef long long ll;
 
 #define MAX 30 // The names will contain no more than 30 capital letters.
 int dp[MAX + 1][MAX + 1];
+ll num[MAX + 1][MAX + 1];
 
 // O(N1 * N2)
 int longestCommonSubsequence(string& arr1, int n1, string& arr2, int n2) {
     for (int i = 0; i <= n1; i++) {
         for (int j = 0; j <= n2; j++) {
-            if (i == 0 || j == 0)
+            if (i == 0 || j == 0) {
                 dp[i][j] = 0;
-            else if (arr1[i - 1] == arr2[j - 1]) {
+                num[i][j] = 1;
+            } else if (arr1[i - 1] == arr2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
+                num[i][j] = num[i - 1][j - 1];
             } else {
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                num[i][j] = 0;
+                if (dp[i - 1][j] >= dp[i][j - 1])
+                    num[i][j] += num[i - 1][j];
+                if (dp[i][j - 1] >= dp[i - 1][j])
+                    num[i][j] += num[i][j - 1];
             }
         }
     }
     return dp[n1][n2];
-}
-
-string getLongestCommonSubsequence(string& arr1, int n1, string& arr2, int n2) {
-    int len = longestCommonSubsequence(arr1, n1, arr2, n2);
-    string result(len, ' ');
-    int i = n1, j = n2;
-    while (i > 0 && j > 0) {
-        if (arr1[i - 1] == arr2[j - 1]) {
-            result[--len] = arr1[i - 1];
-            i--, j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    return result;
 }
 
 int main() {
@@ -57,10 +46,10 @@ int main() {
         cin >> str1 >> str2;
         int len1 = str1.size();
         int len2 = str2.size();
-        longestCommonSubsequence(str1, len1, str2, len2); // O(|str1| * |str2|); |str1|, |str2| <= 30 -> This is complexity of this solution
-        string lcs = getLongestCommonSubsequence(str1, len1, str2, len2);
+        int lcs = longestCommonSubsequence(str1, len1, str2, len2); // O(|str1| * |str2|); |str1|, |str2| <= 30 -> This is complexity of this solution
 
-
+        cout << "Case " << tc << ": ";
+        cout << (len1 + len2 - lcs) << " " << num[len1][len2] << endl;
     }
 
     return 0;
