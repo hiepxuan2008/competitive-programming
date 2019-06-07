78fd4f4681ef95ee1a3b9df2cb0a99b5fffa3805
hiepxuan2008 <hiepxuan2008@gmail.com>
2019-06-07 17:38:23 +0700

    update

--- 25. DP 2 - Longest Common Subsequence/5 [lightoj] 1013 - Love Calculator.cpp
+++ 25. DP 2 - Longest Common Subsequence/5 [lightoj] 1013 - Love Calculator.cpp
@@ -15,21 +15,26 @@ int dp[MAX + 1][MAX + 1];
 ll num[MAX + 1][MAX + 1];
 
 // O(N1 * N2)
-int longestCommonSubsequence(string& arr1, int n1, string& arr2, int n2) {
-    for (int i = 0; i <= n1; i++) {
-        for (int j = 0; j <= n2; j++) {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if (arr1[i - 1] == arr2[j - 1]) {
+int findShortestStringContainOriginalStringsAsSubsequence(string& arr1, int n1, string& arr2, int n2) {
+    for (int i = 0; i <= n1; i++)
+        dp[i][0] = i;
+
+    for (int j = 0; j <= n2; j++)
+        dp[0][j] = j;
+
+    for (int i = 1; i <= n1; i++) {
+        for (int j = 1; j <= n2; j++) {
+            if (arr1[i - 1] == arr2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1;
             }
         }
     }
     return dp[n1][n2];
 }
 
+// O(N1 * N2)
 ll computeWays(string& arr1, int n1, string& arr2, int n2) {
     for (int i = 0; i <= n1; i++) {
         for (int j = 0; j <= n2; j++) {
@@ -38,10 +43,10 @@ ll computeWays(string& arr1, int n1, string& arr2, int n2) {
             else if (arr1[i - 1] == arr2[j - 1]) {
                 num[i][j] = num[i - 1][j - 1];
             } else {
-                if (dp[i - 1][j] > dp[i][j - 1])
-                    num[i][j] = num[i - 1][j];
-                else if (dp[i - 1][j] < dp[i][j - 1])
-                    num[i][j] = num[i][j - 1];
+                if (dp[i - 1][j] < dp[i][j - 1])
+                    num[i][j] = num[i - 1][j]; // pick num ways of shortest string
+                else if (dp[i - 1][j] > dp[i][j - 1])
+                    num[i][j] = num[i][j - 1]; // pick num ways of shortest string
                 else
                     num[i][j] = num[i - 1][j] + num[i][j - 1];
             }
@@ -59,11 +64,11 @@ int main() {
         cin >> str1 >> str2;
         int len1 = str1.size();
         int len2 = str2.size();
-        int lcs = longestCommonSubsequence(str1, len1, str2, len2); // O(|str1| * |str2|); |str1|, |str2| <= 30 -> This is complexity of this solution
+        int shortestLength = findShortestStringContainOriginalStringsAsSubsequence(str1, len1, str2, len2); // O(|str1| * |str2|); |str1|, |str2| <= 30 -> This is complexity of this solution
         ll ways = computeWays(str1, len1, str2, len2); // O(|str1| * |str2|); |str1|, |str2|
 
         cout << "Case " << tc << ": ";
-        cout << (len1 + len2 - lcs) << " " << ways << endl;
+        cout << shortestLength << " " << ways << endl;
     }
 
     return 0;
